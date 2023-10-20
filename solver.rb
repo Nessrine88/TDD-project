9d9354a96797424a8e52acd56f45f7f7a79e0dfd
unknown <macherki.nessrine@yahoo.com>
2023-10-20 22:47:42 +0100

    Write a Failing Test

--- solver.rb
+++
@@ -1,8 +0,0 @@
-class  Solver
-    def factorial(N)
-    if N>0
-        return N*(N-1)
-    end
-else
-    puts "you should put a positive number "
-end
---
+++ solver_spec.rb
@@ -0,0 +1,8 @@
+require 'solver'
+
+describe 'factorial' do
+  it 'returns 1 for input 0' do
+    expect(factorial(0)).to eq(1)
+  end
+
+end
