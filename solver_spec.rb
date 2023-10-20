0bc573069c1467cbfd93e47054448a6a79129f82
unknown <macherki.nessrine@yahoo.com>
2023-10-20 22:58:06 +0100

    add more failing tests

--- solver_spec.rb
+++ solver_spec.rb
@@ -6,3 +6,22 @@ describe 'factorial' do
   end
 
 end
+
+describe 'factorial' do
+    it 'returns 1 for input 0' do
+      expect(factorial(0)).to eq(1)
+    end
+  
+    it 'returns 1 for input 1' do
+      expect(factorial(1)).to eq(1)
+    end
+  
+    it 'returns 120 for input 5' do
+      expect(factorial(5)).to eq(120)
+    end
+  
+    it 'raises an exception for input -2' do
+      expect { factorial(-2) }.to raise_error(ArgumentError, 'Input must be a non-negative integer')
+    end
+  end
+  
